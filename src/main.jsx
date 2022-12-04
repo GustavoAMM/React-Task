53ec52b1c08319aa24650bb679c4f64015e25d21
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-03 23:46:45 -0600

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { TaskContextProvider } from "./context/TaskContext";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
   </React.StrictMode>
-)
+);
