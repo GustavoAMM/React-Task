6a6a97f13577ef61bbb11fc59f9d35d66d87969c
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-03 22:20:30 -0600

    make data and show data

---
+++ src/TaskList.jsx
@@ -0,0 +1,27 @@
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
+
+function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  if (tasks.length === 0) {
+    return <h1>No hay tareas</h1>;
+  }
+
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Primer tarea",
+    description: "Mi primer tarea",
+  },
+  {
+    id: 1,
+    title: "Segunda tarea",
+    description: "Mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "Tercera tarea",
+    description: "Mi tercera tarea",
+  },
+];
